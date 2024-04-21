f015447acdac14e3fe3e44664e18e9507be2f9d7
Alquieri <guilherme.alquieri@gmail.com>
2024-04-20 22:50:19 -0300

    Update Guerreiro.java

--- src/Guerreiro.java
+++ src/Guerreiro.java
@@ -39,7 +39,7 @@ public class Guerreiro extends Personagem {
     @Override
     public String toString() {
        
-        return super.toString() + "\nQuantidade de Força: " + força + "\nNação pertencente" + naçao;
+        return super.toString() + "\nQuantidade de Força: " + força + "\nNação pertencente: " + naçao;
     }
 
  
